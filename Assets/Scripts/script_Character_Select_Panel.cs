ffbc2558447637889d936e23bbf8b863c517559f
JustJacque <35868065+JustJacque@users.noreply.github.com>
2018-01-27 15:27:30 +0000

    Add files via upload

---
+++ Assets/Scripts/script_Character_Select_Panel.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class script_Character_Select_Panel : MonoBehaviour {
+
+public string [] characters;
+public string [] weapons;
+public Text selectionText;
+int characterChoice;
+int weaponChoice;
+
+void Start(){
+
+	characterChoice = 0;
+	weaponChoice = 0;
+	 
+}
+
+public void  CharacterSelect (int characterSelection) {
+	characterChoice = characterSelection;
+	selectionText.text = " Character:" + characters[characterChoice] +"\n Weapon: " + weapons[weaponChoice];
+}
+
+public void WeaponSelect (int weaponSelection) {
+	weaponChoice = weaponSelection;
+	selectionText.text = " Character:" + characters[characterChoice] + "\n Weapon: " + weapons[weaponChoice];
+}
+
+public void Readypressed()	{
+	Debug.Log ("Ready Pressed");
+	if (characterChoice == 0){
+		Debug.Log ("No Character selected");
+		return;
+
+}
+
+	else if (weaponChoice == 0) {
+		Debug.Log ("No Weapon selected");
+		return;
+
+		}
+		else {
+		Debug.Log ("Character and Weapon selected");
+		Debug.Log ("Destroying Character Select Panel");
+		Destroy(this.gameObject);
+
+		}
+	}
+}
---
+++ Assets/Scripts/script_sceneManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class script_sceneManager : MonoBehaviour {
+
+public void LoadScene(string sceneName) {
+
+	Debug.Log ("The Scene loading is: " + sceneName);
+	SceneManager.LoadScene(sceneName);
+	}
+
+
+public void QuitGame (){
+
+	Debug.Log ("The Game is trying to Quit");
+	Application.Quit();
+}
+
+}
